bb2b75d8c1445b419c3438753a88a262885057fb
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2022-09-03 18:56:31 +0700

    add card view layout

--- app/src/main/java/com/huda/dicodingcourse/ListHeroAdapter.kt
+++ app/src/main/java/com/huda/dicodingcourse/ListHeroAdapter.kt
@@ -10,6 +10,11 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
 class ListHeroAdapter(private val listHero : ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>(){
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
         var tvDetail: TextView = itemView.findViewById(R.id.tv_item_detail)
@@ -29,6 +34,11 @@ class ListHeroAdapter(private val listHero : ArrayList<Hero>) : RecyclerView.Ada
             .into(holder.imgPhoto)
         holder.tvName.text = hero.name
         holder.tvDetail.text = hero.detail
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
     }
 
     override fun getItemCount(): Int {
